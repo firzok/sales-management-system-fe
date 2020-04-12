386a59af0712cb2ab9f7d0e717a37fb053eb8bd4
Firzok Nadeem <firzok.nadeem@gmail.com>
2020-04-12 22:25:35 +0500

    Server ip added

--- src/config/rest_endpoints.js
+++ src/config/rest_endpoints.js
@@ -1,9 +1,9 @@
 const PROTOCOL = "http";
 
-// const SERVER_IP = '192.168.100.14';
+const SERVER_IP = "192.168.100.14";
 const PORT = "4000";
 
-const SERVER_IP = "localhost";
+// const SERVER_IP = "localhost";
 // const PORT = '8080';
 
 export const SERVER_PATH = `${PROTOCOL}://${SERVER_IP}:${PORT}`;
